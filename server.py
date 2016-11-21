68d31edbc8fc974ec03362fd499acbc873722f9d
JohnAmadeo <johnamadeo.daniswara@yale.edu>
2016-11-21 13:15:29 -0500

    See last commit

--- server.py
+++ server.py
@@ -89,7 +89,7 @@ def send_message(token, recipient, imageurl):
                     "type":"image",
                     #"payload": { "url": imageurl}
                     # "payload": {"url": 'https://tctechcrunch2011.files.wordpress.com/2011/05/tcdisrupt_tc-9.jpg'}
-                    "payload": {"url": 'https://emojiverse2-secondary.blob.core.windows.net/imgstore/img719e1182-6642-4737-8cb2-7e469e3c7bdc.jpg'}
+                    "payload": {"url": 'https://emojiverse2.blob.core.windows.net/imgstore/emoji.jpg'}
                 }
             }
         }),
@@ -97,6 +97,7 @@ def send_message(token, recipient, imageurl):
 
     if r.status_code != requests.codes.ok:
         print(r.text)
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     app.run()
